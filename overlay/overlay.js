f2c8dd025613c42ef082940b079ffb1378c6929c
Chris Lydic <cxl9041@rit.edu>
2019-02-22 17:30:34 -0500

    UPD new code for overlay

--- overlay/overlay.js
+++ overlay/overlay.js
@@ -5,21 +5,173 @@ const bottomRight = `<div class="item" style="bottom: 0; right: 0;" id="glyph4">
 const listItem = `<div class="item" style="bottom: {bottom}; right: {right};" id="glyph{id}">{sym}</div>`;
 const glphTitle = `<a class="item" id="glphTitle" href="https://gl-ph.com">gl-ph</a>`;
 
-let types = {
-    "title": {char: "*", id: "#glyph1", event: null},
-    "interview": {char: "+", id: "", event: null},
-    "critical": {char: "&amp;", id: "", event: null},
-    "extra": {char: "~", id: "#glyph4", event: null},
-    "fun": {char: "!", id: "", event: null},
-    "fun2": {char: "#", id: "", event: null},
-    "fun3": {char: "=", id: "", event: null},
-    "fun4": {char: "?", id: "", event: null},
+// stores the mapping of content type to its glyph
+let typeModels = {
+    "title": {char: "*", id: "#glyph1", event: null, callback: null},
+    "interview": {char: "+", id: "", event: null, callback: null},
+    "critical": {char: "&amp;", id: "", event: null, callback: null},
+    "extra": {char: "~", id: "#glyph4", event: null, callback: null},
+    "fun": {char: "!", id: "", event: null, callback: null},
+    "fun2": {char: "#", id: "", event: null, callback: null},
+    "fun3": {char: "=", id: "", event: null, callback: null},
+    "fun4": {char: "?", id: "", event: null, callback: null},
 };
 
-let data = {};
+class Overlay {
+    constructor(itemCount, color, outline) {
+        this.itemCount = itemCount;
+        this.color = color;
+        this.outline = outline;
+        this.active = 'title';
+        
+        this.types = {};
 
-// keep track of current state
-let active = 'title';
+        if (this.itemCount > 4) {
+            this.types['extra'] = typeModels['extra'];
+        }
+    }
+
+    addContent(type, callback) {
+        this.types[type] = typeModels[type];
+        this.types[type].callback = callback;
+    }
+    
+    callEvent(type) {
+        this.types[type].event();
+    }
+    
+    toggleList() {
+        for (let i = 5; i <= this.itemCount + 1; i++) {
+            $('#glyph' + i).toggle();
+        }
+    }
+    
+    addExpandingList(glyphs) {
+        let id, data;
+        let bottom = 100;
+        let right = 100;
+
+        // a portrait orientation can have issues with the list positioning
+        let isPortrait = (window.innerHeight / window.innerWidth) > 1;
+        
+        if (isPortrait) {
+            right = 50;
+        }
+
+        $('body').prepend(bottomRight.replace("{sym}", this.types["extra"].char));
+        $(this.types["extra"].id).click(this.toggleList.bind(this));
+        
+        for (let i = 0; i < glyphs.length; i++) {
+            id = 5 + i;
+            this.types[glyphs[i]].id = '#glyph' + id;
+
+            data = listItem.replace("{sym}", this.types[glyphs[i]].char)
+                .replace("{id}", id)
+                .replace("{bottom}", bottom)
+                .replace("{right}", right);
+
+            $('body').prepend(data);
+            
+            $(this.types[glyphs[i]].id).toggle();
+            
+            this.createHandler(glyphs[i]);
+            
+            bottom += 100;
+
+            if (isPortrait) {
+                right += 50;
+            } else {
+                right += 100;
+            }
+        }
+    }
+    
+    createHandler(type) {
+        let id = this.types[type].id;
+        
+        let callback = (function() {
+            $("div.item").css("text-decoration", "");
+            
+            if (this.itemCount > 4) {
+                if ($('#glyph5').css('display') !== 'none') {
+                    this.toggleList();
+                }
+            }
+
+            if (this.active === type) {
+                // reset to title page
+                this.active = 'title';
+                $(this.types['title'].id).css("text-decoration", "underline");
+                
+                this.types['title'].callback();
+            } else {
+                this.active = type;
+                $(this.types[type].id).css("text-decoration", "underline");
+                
+                this.types[type].callback();
+            }
+        }).bind(this);
+        
+        $(id).click(callback);
+        this.types[type].event = callback;
+    }
+    
+    render() {
+        $('body').prepend(glphTitle);
+        $('.item#glphTitle').mouseenter(function() {
+            $('.item#glphTitle').text('gl~ph');
+        });
+        $('.item#glphTitle').mouseleave(function() {
+            $('.item#glphTitle').text('gl-ph');
+        });
+        
+        let glyphs = Object.keys(this.types).filter(name => name !== "title" && name !== "extra");
+        glyphs = shuffle(glyphs);
+        
+        $('body').prepend(topLeft);
+        this.createHandler('title');
+
+        if (this.itemCount > 4) {
+            // button for toggling the list of extra glyphs
+            this.addExpandingList(glyphs.slice(2));
+            // only use the first two values in loop below
+            glyphs = glyphs.slice(0, 2);
+        }
+
+        for (let i = 0; i < glyphs.length; i++) {
+            let type = glyphs[i];
+            this.types[type].id = "#glyph" + (i + 2);
+
+            let data = "";
+
+            if (i === 0) {
+                data = topRight.replace("{sym}", this.types[type].char);
+            } else if (i === 1) {
+                data = bottomLeft.replace("{sym}", this.types[type].char);
+            } else if (i === 2) {
+                data = bottomRight.replace("{sym}", this.types[type].char);
+            } else {
+                console.error("Unexpected number of extras");
+            }
+
+            $('body').prepend(data);
+            this.createHandler(type);
+        }
+        
+        $(".item").css("color", this.color);
+        $(".item").css("text-shadow", `
+            -1px -1px 0 ` + this.outline + `,
+            1px -1px 0 ` + this.outline + `,
+            -1px 1px 0 ` + this.outline + `,
+            1px 1px 0 ` + this.outline + `,
+            -2px 0 0 ` + this.outline + `,
+            2px 0 0 ` + this.outline + `,
+            0 2px 0 ` + this.outline + `,
+            0 -2px 0 ` + this.outline);
+        
+        $(this.types['title'].id).css("text-decoration", "underline");
+    }
+}
 
 /**
 The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle.
@@ -41,141 +193,4 @@ function shuffle(array) {
     }
 
     return array;
-}
-
-let addNew = function(type, callback) {
-    data[type] = callback;
-}
-
-let toggleList = function(glyphs) {
-    for (let i = 0; i < glyphs.length; i++) {
-        id = 5 + i;
-        $('#glyph' + id).toggle();
-    }
-}
-
-let addListButtons = function(glyphs) {
-    let id;
-    let bottom = 100;
-    let right = 100;
-    
-    for (let i = 0; i < glyphs.length; i++) {
-        id = 5 + i;
-        types[glyphs[i]].id = '#glyph' + id;
-        $('body').prepend(listItem.replace("{sym}", types[glyphs[i]].char).replace("{id}", id).replace("{bottom}", bottom).replace("{right}", right));
-        
-        $(types[glyphs[i]].id).toggle();
-        
-        createHandler(glyphs, glyphs[i]);
-        
-        bottom += 100;
-        right += 100;
-    }
-}
-
-let createHandler = function(glyphs, type) {
-    let id = types[type].id;
-    
-    let callback = function() {
-        $("div.item").css("text-decoration", "");
-        
-        if (active === type) {
-            // reset to title page
-            active = 'title';
-            $(types['title'].id).css("text-decoration", "underline");
-            
-            data['title']();
-        } else {
-            active = type;
-            $(types[type].id).css("text-decoration", "underline");
-            
-            data[type]();
-        }
-    };
-    
-    if (glyphs.length > 0) {
-        let wrapCallback = function(values, callbackFunc) {
-            if ($('#glyph5').css('display') !== 'none') {
-                toggleList(values);
-            }
-            callbackFunc();
-        }.bind(null, glyphs, callback);
-        
-        $(id).click(wrapCallback);
-        types[type].event = wrapCallback;
-    } else {
-        $(id).click(callback);
-        types[type].event = callback;
-    }
-}
-
-// takes in base color and outline color
-let render = function(color, outline) {
-    $('body').prepend(glphTitle);
-    $('.item#glphTitle').mouseenter(function() {
-        $('.item#glphTitle').text('gl~ph');
-    });
-    $('.item#glphTitle').mouseleave(function() {
-        $('.item#glphTitle').text('gl-ph');
-    });
-    
-    if (color === undefined) {
-        color = "black";
-    }
-    if (outline === undefined) {
-        outline = "white";
-    }
-    
-    glyphs = Object.keys(data).filter(name => name !== "title");
-    glyphs = shuffle(glyphs);
-    
-    let typeOne = glyphs.pop();
-    let typeTwo = glyphs.pop();
-    
-    if (glyphs.length > 1) {
-        // button for toggling the list of extra glyphs
-        $('body').prepend(bottomRight.replace("{sym}", types["extra"].char));
-        $(types["extra"].id).click(toggleList.bind(null, glyphs));
-        addListButtons(glyphs);
-    } else if (glyphs.length > 0) {
-        // in this case there are three extra glyphs
-        let typeTemp = glyphs.pop();
-        types[typeTemp].id = "#glyph4";
-        $('body').prepend(bottomRight.replace("{sym}", types[typeTemp].char));
-        createHandler(glyphs, typeTemp);
-    }
-    
-    $('body').prepend(topLeft);
-    $(types['title'].id).click();
-    
-    createHandler(glyphs, 'title');
-    
-    if (typeOne !== undefined) {
-        types[typeOne].id = "#glyph2";
-        $('body').prepend(topRight.replace("{sym}", types[typeOne].char));
-        createHandler(glyphs, typeOne);
-    }
-    
-    if (typeTwo !== undefined) {
-        types[typeTwo].id = "#glyph3";
-        $('body').prepend(bottomLeft.replace("{sym}", types[typeTwo].char));
-        createHandler(glyphs, typeTwo);
-    }
-    
-    $(".item").css("color", color);
-    $(".item").css("text-shadow", `
-        -1px -1px 0 ` + outline + `,
-        1px -1px 0 ` + outline + `,
-        -1px 1px 0 ` + outline + `,
-        1px 1px 0 ` + outline + `,
-        -2px 0 0 ` + outline + `,
-        2px 0 0 ` + outline + `,
-        0 2px 0 ` + outline + `,
-        0 -2px 0 ` + outline);
-    
-    $(types['title'].id).css("text-decoration", "underline");
-}
-
-let callEvent = function(type) {
-    types[type].event();
 }
